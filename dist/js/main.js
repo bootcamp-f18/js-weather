459124a11ab2ae875331a4a696d5e53a72ef7589
Cmeredith <tina.wheezy@gmail.com>
2018-10-01 14:28:34 -0400

    following along in class

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,21 @@
 // This is main.js
+let zipcode = document.getElementById('zipcodeInput');
+let weatherButton = document.getElementById('weatherButton');
+let appId = '58e92c763df5499a2c9ae20da806e2dc';
 
-document.onreadystatechange = function ()  {
-  if(document.readyState == "interactive") {
-    document.getElementById('weatherButton').onclick = getWeather;
+document.onreadystatechange = function () {
+  if (document.readyState == "interactive") {
+    weatherButton.onclick = getWeather;
   }
-}
+};
 
 function getWeather() {
-
-  let
+  // Get zipcode
+  // -> zipcode.value
+  // Format a url
+  let url = 'http://api.openweathermap.org/data/2.5/weather?zip=<zipcode>,us&appid=<appid>';
+  url = url.replace("<zipcode>", zipcode.value);
+  url = url.replace("<appid>", appId);
+  console.log(url); // Fetch from the url
+  // Fill in the page
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,66 @@
 // This is main.js
+
+let zipcode = document.getElementById('zipcodeInput');
+let weatherButton = document.getElementById('weatherButton');
+// below is the variable that holds our API key
+let appId = '58e92c763df5499a2c9ae20da806e2dc';
+let apiRequest = new XMLHttpRequest();
+let output = document.getElementById('output');
+let error = document.getElementById('error');
+
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        weatherButton.onclick = getWeather;
+    }
+}
+
+function getWeather() {
+
+    // Get zipcode
+    // -> zipcode.value
+
+    // Format a url
+    let url = 'http://api.openweathermap.org/data/2.5/weather?zip=<zipcode>,us&appid=<appid>';
+    url = url.replace("<zipcode>", zipcode.value);
+    url = url.replace("<appid>", appId);
+
+    //fetch from the url
+    l
+    apiRequest.onload = onSuccess;
+    apiRequest.onerror = onError;
+    apiRequest.open('get', url, true);
+    apiRequest.send();
+    // Fetch from the url
+
+
+    // Fill in the page
+
+
+
+
+}
+
+
+function onError() {
+  //Turn off the output div
+  output.style.display = 'none';
+  //Turn on the error div
+  error.style.display = 'block';
+  // Update the text inside error
+  document.querySelector('#error div').innerHTML = "An error has occurred. Please try again";
+
+
+}
+
+function on onSuccess() {
+ if(apiRequest.statusText == "200") {
+   console.log("Request should return good data.");
+
+   //Start filling out the output div.
+
+ }
+    else {
+      console.log("Request returned something other than a success. Process this as an error.");
+    }
+}
